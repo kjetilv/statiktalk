5afeb44cf29302da834efb6ec48ef7cd9299c810
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-07-15 10:52:40 +0200

    Exotic start/end

--- processor/src/main/kotlin/com/github/kjetilv/statiktalk/SenderTemplate.kt
+++ processor/src/main/kotlin/com/github/kjetilv/statiktalk/SenderTemplate.kt
@@ -3,49 +3,49 @@ package com.github.kjetilv.statiktalk
 internal val senderTemplate
     get() =
         """
-package <packidge>
+package ⎨packidge⎬
 
 import com.github.kjetilv.statiktalk.api.SendMediatorBase
 import no.nav.helse.rapids_rivers.RapidsConnection
 
 /*
   KMessage(
-    packidge          : <packidge>
-    service           : <service>
-    serviceCc         : <serviceCc>
-    serviceName       : <serviceName>
-    requireServiceName: <requireServiceName>
-    parameters        : <parameters:{parameter|<parameter> }>
-    additionalKeys    : <additionalKeys:{additionalKey|<additionalKey> }>
-    contextual        : <contextual>
-    contextualNonNull : <contextualNonNull>
+    packidge          : ⎨packidge⎬
+    service           : ⎨service⎬
+    serviceCc         : ⎨serviceCc⎬
+    serviceName       : ⎨serviceName⎬
+    requireServiceName: ⎨requireServiceName⎬
+    parameters        : ⎨parameters:{parameter|⎨parameter⎬ }⎬
+    additionalKeys    : ⎨additionalKeys:{additionalKey|⎨additionalKey⎬ }⎬
+    contextual        : ⎨contextual⎬
+    contextualNonNull : ⎨contextualNonNull⎬
   )
-  hasParams         : <hasParams>
-  hasAdditionalKeys : <hasAdditionalKeys>
-  contextClass      : <contextClass>
+  hasParams         : ⎨hasParams⎬
+  hasAdditionalKeys : ⎨hasAdditionalKeys⎬
+  contextClass      : ⎨contextClass⎬
 */
 
-fun RapidsConnection.new<service>(): <service> =
-    <service>SendMediator(this)
+fun RapidsConnection.new⎨service⎬(): ⎨service⎬ =
+    ⎨service⎬SendMediator(this)
 
-private class <service>SendMediator(
+private class ⎨service⎬SendMediator(
     rapidsConnection: RapidsConnection
-) : SendMediatorBase(rapidsConnection), <service> {
+) : SendMediatorBase(rapidsConnection), ⎨service⎬ {
 
-    override fun <serviceName>(
-<parameters:{parameter|
-        <parameter>: String,
-        }><if(contextual)
-        >        context: <contextClass><if(contextualNonNull)><else>?<endif>
-        <else><endif>
+    override fun ⎨serviceName⎬(
+⎨parameters:{parameter|
+        ⎨parameter⎬: String,
+        }⎬⎨if(contextual)
+        ⎬        context: ⎨contextClass⎬⎨if(contextualNonNull)⎬⎨else⎬?⎨endif⎬
+        ⎨else⎬⎨endif⎬
     ) {
-        send<if(contextualNonNull)>1<else>0<endif>(
-            <if(contextual)>context<else>null<endif>,
-            "@event_name" to "<service>_<serviceName>",
-<parameters:{parameter|
-            "<parameter>" to <parameter>,
+        send⎨if(contextualNonNull)⎬1⎨else⎬0⎨endif⎬(
+            ⎨if(contextual)⎬context⎨else⎬null⎨endif⎬,
+            "@event_name" to "⎨service⎬_⎨serviceName⎬",
+⎨parameters:{parameter|
+            "⎨parameter⎬" to ⎨parameter⎬,
             }
->       )
+⎬       )
     }
 }
 """.trimIndent()
