208aa81798794d5f82724597ef69464aafd0bd0b
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-12-09 17:49:48 +0100

    Having some fun

--- api/src/main/kotlin/com/github/kjetilv/statiktalk/api/SendMediatorBase.kt
+++ api/src/main/kotlin/com/github/kjetilv/statiktalk/api/SendMediatorBase.kt
@@ -6,16 +6,21 @@ import no.nav.helse.rapids_rivers.RapidsConnection
 
 private const val eventNameKey = "@event_name"
 
+@Suppress("unused")
 abstract class SendMediatorBase(private val eventName: String? = null, private val connection: RapidsConnection) {
 
-    protected fun send0(ctx: Context?, vararg contents: Pair<String, Any?>) =
-            contents.toMap().also { ctx.publish(it) }
+    protected fun send0(ctx: Context?, vararg contents: Pair<String, Any?>) = send0(ctx, contents.toList())
 
-    protected fun send1(ctx: Context, vararg contents: Pair<String, Any?>) =
-            contents.toMap().also { ctx.publish(it) }
+    protected fun send0(ctx: Context?, contents: List<Pair<String, Any?>>) =
+        contents.toMap().also { ctx.publish(it) }
+
+    protected fun send1(ctx: Context, vararg contents: Pair<String, Any?>) = send1(ctx, contents.toList())
+
+    protected fun send1(ctx: Context, contents: List<Pair<String, Any?>>) =
+        contents.toMap().also { ctx.publish(it) }
 
     private fun Context?.publish(map: Map<String, Any?>) =
-            (this as? DefaultContext)?.publishUpdated(map) ?: publishNew(map)
+        (this as? DefaultContext)?.publishUpdated(map) ?: publishNew(map)
 
     private fun DefaultContext.publishUpdated(map: Map<String, Any?>) {
         nonNullValues(map).forEach { (key, value) ->
@@ -25,11 +30,11 @@ abstract class SendMediatorBase(private val eventName: String? = null, private v
     }
 
     private fun publishNew(map: Map<String, Any?>) =
-            applyEventName(newMessage(nonNullValues(map))).toJson().run(connection::publish)
+        applyEventName(newMessage(nonNullValues(map))).toJson().run(connection::publish)
 
     private fun applyEventName(jsonMessage: JsonMessage) =
-            jsonMessage.also { eventName?.also { eventName -> it[eventNameKey] = eventName } }
+        jsonMessage.also { eventName?.also { eventName -> it[eventNameKey] = eventName } }
 
     private fun nonNullValues(keys: Map<String, Any?>) =
-            keys.filterValues { it != null }.mapValues { (_, it) -> it!! }
+        keys.filterValues { it != null }.mapValues { (_, it) -> it!! }
 }
--- processor/src/main/kotlin/com/github/kjetilv/statiktalk/processor/templates/ST.kt
+++ processor/src/main/kotlin/com/github/kjetilv/statiktalk/processor/templates/ST.kt
@@ -6,6 +6,7 @@ import com.github.kjetilv.statiktalk.processor.KService
 import org.stringtemplate.v4.ST
 import java.math.BigDecimal
 import java.math.BigInteger
+import kotlin.math.max
 
 internal fun String.source(service: KService, messages: List<KMessage>) =
     try {
@@ -20,12 +21,23 @@ internal fun String.source(service: KService, messages: List<KMessage>) =
     }.trim().let { adorn(it) }
 
 private fun adorn(code: String) =
-    code.split("\n").let { lines ->
-        lines.maxOf { it.length }.let { maxLength ->
-            lines.mapIndexed { i, line -> suffixed(i, line, maxLength) }
-                .framed(maxLength).joinToString("\n")
+    code.split("\n")
+        .map { it.trimEnd() }
+        .let { lines ->
+            lines.indexOfFirst { line -> line.startsWith("@file") }
+                .let { header ->
+                    lines.maxOf { it.length }
+                        .let { len -> (len * 1.1).toInt() }
+                        .let { len ->
+                            lines.mapIndexed { i, line ->
+                                suffixed(i, line, len, header)
+                            }
+                                .framed(len)
+                        }
+                        .joinToString("\n")
+
+                }
         }
-    }
 
 private const val STATICTALK = "statictalk "
 
@@ -33,20 +45,54 @@ private const val PRE = "/* $STATICTALK */ "
 
 private const val POST = " // DO NOT TOUCH"
 
-private fun suffixed(index: Int, line: String, maxLength: Int) =
+private fun suffixed(index: Int, line: String, maxLength: Int, header: Int) =
     (maxLength - line.length).let { buffer ->
-        PRE.replace(STATICTALK, shuffled(index, STATICTALK)) + line + " ".repeat(buffer) + POST
+        max(0, line.takeWhile(Char::isWhitespace).length).let { preamble ->
+            (header > index).let { inHeader ->
+                shuffled(index, STATICTALK, preamble, inHeader).let { shuffled ->
+                    (if (inHeader) line else line.substring(preamble)).let { remainingLine ->
+                        PRE.replace(STATICTALK, shuffled) + remainingLine + emptySpace(line, buffer, inHeader) + POST
+                    }
+                }
+            }
+        }
+    }
+
+private fun emptySpace(
+    line: String,
+    buffer: Int,
+    inHeader: Boolean
+) =
+    if (inHeader) " ".repeat(buffer)
+    else line.endsWith(" ").let { spaced ->
+        (if (spaced) 6 else 7).let { preamble ->
+            if (buffer in 0..preamble) " ".repeat(buffer)
+            else (if (spaced) "" else " ") + "/* ${" ".repeat(buffer - preamble)} */"
+        }
     }
 
 private fun List<String>.framed(maxLength: Int) =
     maxOf(0, maxLength + PRE.length + POST.length - 6).let { length ->
         ("/* " + "–".repeat(length) + " */").let { frame ->
-            listOf(frame) + this + listOf(frame)
+            listOf("// @formatter:off") + listOf(frame) + this + listOf(frame) + listOf("// @formatter:on")
         }
     }
 
-fun shuffled(index: Int, str: String) =
-    str.substring(index % str.length) + str.substring(0, index % str.length)
+fun shuffled(index: Int, str: String, preamble: Int, inHeader: Boolean) =
+    (index % str.length).let { pos ->
+        str.substring(pos) + str.substring(0, pos)
+    }.let { base ->
+        if (inHeader) base else base + repeated(base, preamble)
+    }
+
+private fun repeated(base: String, preamble: Int): String {
+    val len = base.length
+    return base.substring(0, minOf(preamble, len)) + (if (preamble >len) {
+        repeated(base, preamble - len)
+    } else {
+        ""
+    })
+}
 
 private fun imports(messages: List<KMessage>) =
     messages.flatMap { message -> message.keys.map { key -> key.type } }
